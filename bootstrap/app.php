bbaab8729f485516a880b7d22e7a0e2af9888d16
hadiseh firouzabadi <82514441+hadiseFra@users.noreply.github.com>
2025-05-22 15:50:49 +0330

    feat: install sanctum package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,38 @@
 <?php
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\RateLimiter;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: '',
         commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
+        then: function () {
+            RateLimiter::for('api', function (Illuminate\Http\Request $request) {
+                return Limit::perMinute(90)->by(optional($request->user())->id ?: $request->ip());
+            });
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->priority([
+            'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+            'can' => \Illuminate\Auth\Middleware\Authorize::class,
+            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        ]);
+        
+        $middleware->group('api', [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'throttle:api',
+            'auth:sanctum',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
