249e2307297a10249b721cbadcf0f888be8e6b57
hadiseh firouzabadi <82514441+hadiseFra@users.noreply.github.com>
2025-05-29 13:29:46 +0330

    refactor: changed routes structure for better readability.

--- routes/api.php
+++ routes/api.php
@@ -11,26 +11,23 @@ Route::withoutMiddleware(['auth:sanctum'])->group(function () {
     });
 });
 
-Route::prefix('v1')->name('users.')->middleware(['auth:sanctum'])->group(function () {
-
-    Route::prefix('categories')->group(function () {
-
-        Route::prefix('{user}')->group(function () {
-
+Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
+    Route::prefix('users/{user}')->name('users.')->group(function () {
+        /**
+         *  User - Categories routes
+         * Example: v1/users/:id/categories
+         */
+        Route::prefix('/categories')->name('categories.')->group(function () {
             Route::get('/', [CategoryController::class, 'index'])->name('index');
             Route::post('/', [CategoryController::class, 'store'])->name('store');
+            Route::get('/search', [CategoryController::class, 'search'])->name('search');
 
             Route::prefix('{category}')->group(function () {
                 Route::get('/', [CategoryController::class, 'show'])->name('show');
                 Route::put('/', [CategoryController::class, 'update'])->name('update');
                 Route::delete('/', [CategoryController::class, 'destroy'])->name('destroy');
             });
-
         });
-
     });
-
 });
-
-
 
