134e8d09c511e1a25f417e8c227105e3418fb680
hadiseh firouzabadi <82514441+hadiseFra@users.noreply.github.com>
2025-06-01 11:50:53 +0330

    feat: user and category resources is added.

---
+++ app/Http/Resources/User/Category/CategoryResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\User\Category;
+
+use App\Http\Resources\User\User\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'user' => UserResource::make($this->user),
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
